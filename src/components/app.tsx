b7a145701d08803bcbf09c9be72acaccaced2160
vecejlo <vecejlo@gmail.com>
2019-10-22 22:57:10 +0300

    start codding, add fetch

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,68 @@
 import * as React from "react"
 
-export interface HelloProps { compiler: string; framework: string; }
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
-export class Hello extends React.Component<HelloProps, {}> {
+//TODO нужно тут все подчистить и вправить на место, сейчас не работает, потому что я еще не доконца заюзал TS
+
+interface MyProps {
+    data: object,
+    Valute: object,
+    EUR: object
+}
+
+interface MyState {
+    data: object,
+    Valute: object,
+    EUR: object
+}
+
+export class Hello extends React.Component<MyProps, MyState> {
+    public state: MyState = {
+        data: {
+            Valute: {
+                EUR: {}
+            }
+        }
+    }
+    // state = {
+
+    // }
+
+    componentDidMount(): void {
+        fetch('https://www.cbr-xml-daily.ru/daily_json.js', {
+            method: 'get',
+            // mode: 'no-cors'
+        })
+            .then(result => result.json())
+            .then(data => this.setState({data}))
+    }
+
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+        console.log('--this.state.data', this.state.data)
+        const valute: object = this.state.data.Valute
+        const EUR: object = valute.EUR
+        return (
+            <main>
+                <h1>call data and show</h1>
+                <small>{new Date().toDateString()}</small>
+                <table>
+                    <thead>
+                    <tr>
+                        <td>Валюта</td>
+                        <td>Курс</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>
+                            {'EUR'}
+                        </td>
+                        <td>
+                            {EUR}
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </main>
+        )
     }
 }
